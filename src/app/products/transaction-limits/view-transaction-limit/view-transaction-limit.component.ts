d6c397f2f2acd95d56ba836bf981419bd0ad7640
Hitesh Prajapati <hiteshkumar.prajapati@gmail.com>
2025-08-19 14:16:56 +0530

    View Transaction limit page

--- src/app/products/transaction-limits/view-transaction-limit/view-transaction-limit.component.ts
+++ src/app/products/transaction-limits/view-transaction-limit/view-transaction-limit.component.ts
@@ -9,6 +9,7 @@ import { ProductsService } from 'app/products/products.service';
 import { TranslateService } from '@ngx-translate/core';
 import { DeleteDialogComponent } from '../../../shared/delete-dialog/delete-dialog.component';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { YesnoPipe } from '../../../pipes/yesno.pipe';
 import { STANDALONE_SHARED_IMPORTS } from 'app/standalone-shared.module';
 
 /**
@@ -20,46 +21,25 @@ import { STANDALONE_SHARED_IMPORTS } from 'app/standalone-shared.module';
   styleUrls: ['./view-transaction-limit.component.scss'],
   imports: [
     ...STANDALONE_SHARED_IMPORTS,
-    FaIconComponent
+    FaIconComponent,
+    YesnoPipe
   ]
 })
 export class ViewTransactionLimitComponent {
-  /** Collateral Data */
-  collateralData: any;
+  transactionLimitData: any;
 
-  /**
-   * Retrieves the Collateral Data from 'resolve.
-   * @param {ProductsService} productsService Products Service.
-   * @param {ActivatedRoute} route Activated Route.
-   * @param {Router} router Router for navigation.
-   * @param {MatDialog} dialog Dialog reference.
-   * @param {TranslateService} translateService Translate Service.
-   */
   constructor(
-    private productsService: ProductsService,
-    private route: ActivatedRoute,
     private router: Router,
-    private dialog: MatDialog,
-    private translateService: TranslateService
+    private route: ActivatedRoute
   ) {
-    this.route.data.subscribe((data: { collateral: any }) => {
-      this.collateralData = data.collateral;
+    this.route.data.subscribe((data: { transactionLimit: any }) => {
+      this.transactionLimitData = data.transactionLimit;
     });
   }
 
-  /**
-   * Deletes the collateral and redirects to collaterals
-   */
-  deleteCollateral() {
-    const deleteCollateralDialogRef = this.dialog.open(DeleteDialogComponent, {
-      data: { deleteContext: this.translateService.instant('labels.text.Collateral') + ' ' + this.collateralData.id }
-    });
-    deleteCollateralDialogRef.afterClosed().subscribe((response: any) => {
-      if (response.delete) {
-        this.productsService.deleteCollateral(this.collateralData.id).subscribe(() => {
-          this.router.navigate(['/products/collaterals']);
-        });
-      }
-    });
+  /* ngOnInit(): void {} */
+
+  editTransactionLimit() {
+    this.router.navigate(['edit'], { relativeTo: this.route });
   }
 }
