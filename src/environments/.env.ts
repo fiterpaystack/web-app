640de30a9f043d1442bfa1b85c268301b576375e
Kelvin Ikome <kvnikome@gmail.com>
2025-09-02 12:34:24 +0100

    update client listing labels

--- src/environments/.env.ts
+++ src/environments/.env.ts
@@ -2,8 +2,8 @@
 /* tslint:disable */
 export default {
   'mifos_x': {
-    'version': '250829',
-    'hash': '158aadda'
+    'version': '250902',
+    'hash': '5590680f'
   },
   'allow_switching_backend_instance': true
 };
